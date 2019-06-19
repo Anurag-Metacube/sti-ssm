fd564d842237990c1828ae940c3f47fb595d7b2c
Anurag Kumawat <anurag.kumawat@metacube.com>
2019-06-19 17:37:48 +0530

    Initial Structure

---
+++ src/main/java/com/sti/ssm/StiSsmApplication.java
@@ -0,0 +1,13 @@
+package com.sti.ssm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StiSsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StiSsmApplication.class, args);
+	}
+
+}
