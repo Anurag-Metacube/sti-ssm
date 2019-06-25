4f48626bbcc69edeedea14c1393c8c1c9b3f957a
Bhagyesh Jain <bhagyesh.jain@metacube.com>
2019-06-25 17:01:25 +0530

    Logger Configuration - Using "@Slf4j" annotation

--- src/main/java/com/sti/ssm/controller/StiSsmController.java
+++ src/main/java/com/sti/ssm/controller/StiSsmController.java
@@ -1,20 +1,17 @@
 package com.sti.ssm.controller;
 
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class StiSsmController {
-
-    @Autowired
-    private Logger logger;
 
     @GetMapping(value = "/test")
     public @ResponseBody String test() {
-        logger.debug("testing");
+        log.debug("testing");
         return "Test";
     }
 }
