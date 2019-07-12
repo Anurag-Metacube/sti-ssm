12c00f34d728ed4de4e434c324b295cd2b6aa99e
Anurag Kumawat <anurag.kumawat@metacube.com>
2019-07-12 17:31:56 +0530

    Integration with ping federate sso

--- src/main/java/com/sti/ssm/controller/IndexController.java
+++ src/main/java/com/sti/ssm/controller/IndexController.java
@@ -2,15 +2,26 @@ package com.sti.ssm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sti.ssm.annotation.SAMLUser;
+import com.sti.ssm.config.security.SAMLUserDetails;
 
 @Controller
 public class IndexController extends BaseController {
 
 
-	@RequestMapping(value = {"/", "/index", "/logged-in"})
+	@RequestMapping("/index")
 	public String index() {
-		return "logged-in";
+		return "index";
 	}
 
+    @RequestMapping("/home")
+    public ModelAndView home(@SAMLUser SAMLUserDetails user) {
+        ModelAndView homeView = new ModelAndView("home");
+        homeView.addObject("userId", user.getUsername());
+        homeView.addObject("samlAttributes", user.getAttributes());
+        return homeView;
+    }
 	
 }
